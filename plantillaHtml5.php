f16ab3c6ac8fb5219f3e88601ded8364df11dad3
zhuoIC <nicolimonar@gmail.com>
2017-12-13 10:20:49 +0100

    Plantilla HTML con Boostrap

--- plantillaHtml5.php
+++ plantillaHtml5.php
@@ -4,10 +4,15 @@ print " <!DOCTYPE html>
     <head>
         <title>Titulo de la WEB</title>
         <meta charset=\"utf-8\"/>
+        <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">
+        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
+        <link href=\"css/bootstrap.min.css\" rel=\"stylesheet\" media=\"screen\">
     </head>
-    <body> ";
+    <body> 
+        <script src=\"http://code.jquery.com/jquery.js\"></script>
+        <script src=\"js/bootstrap.min.js\"></script>";
     $titulo = "Primera página HTML";
-    print "<h1>$titulo</h1>";
+    print "<h1>".$titulo."</h1>";
 print "
     </body>
 </html>
